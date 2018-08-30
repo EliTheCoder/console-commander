73aef039d574b07d445e86e9125108c5c1460409
Liam <Liam.taylor@outlook.com>
2018-08-30 15:00:21 +0100

    map simplicities

--- assets/maps.js
+++ assets/maps.js
@@ -1,6 +1,6 @@
 function Map(map){
     if(map == '1'){
-        return [
+        ThisMap = [
             [1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1],
@@ -10,7 +10,7 @@ function Map(map){
             [1, 1, 1, 1, 1, 1, 1],
         ];
     } else if(map == '2'){
-        return [
+        ThisMap = [
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 1, 1, 1, 0, 0],
             [0, 1, 1, 1, 1, 1, 0],
@@ -20,7 +20,7 @@ function Map(map){
             [0, 0, 0, 0, 0, 0, 0],
         ];
     } else if(map == '3'){
-        return [
+        ThisMap = [
             [1, 1, 1, 0, 1, 1, 1],
             [1, 1, 1, 0, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1],
@@ -30,7 +30,7 @@ function Map(map){
             [1, 1, 1, 0, 1, 1, 1],
         ];
     } else if (map == '4'){
-        return [
+        ThisMap = [
             [1, 1, 0, 0, 0, 1, 1],
             [1, 1, 1, 0, 1, 1, 1],
             [0, 1, 1, 1, 1, 1, 0],
@@ -40,4 +40,18 @@ function Map(map){
             [1, 1, 0, 0, 0, 1, 1],
         ];
     }
+    var html = '';
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+    html += '<table style="display: block; margin: auto auto; max-width: 500px; background-color: #f1f1f1; border: solid 2px rgba(0,0,0,0.5);">';
+        html += '<tbody style="display: block; width: 100%;">';
+            for(row = 0; row < ThisMap.length; row++){
+                html += '<tr style="display: block; width: 100%;">';
+                    for(column = 0; column < ThisMap[row].length; column++){
+                        html += '<td id="' + alphabet[row] + (column + 1) + '" ' + ((ThisMap[row][column] == 0) ? 'class="impassible"' : '') + ' style="width: 72px; text-align: center; padding: 10px; border: solid 1px black;">' + alphabet[row] + (column + 1) + '</td>';
+                    }
+                html += '</tr>';
+            }
+        html += '</tbody>';
+    html += '</table>';
+    $('body').append(html);
 }
--- gameboot.js
+++ gameboot.js
@@ -1,22 +1,7 @@
 function setup() {
 
     // prepare our map ((need to implement this with p5 canvas shite))
-    var html = '';
-    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-    
-    ThisMap = Map('4'); // map 1, 2, 3, 4 have been created.
-    html += '<table style="display: block; margin: auto auto; max-width: 500px; background-color: #f1f1f1; border: solid 2px rgba(0,0,0,0.5);">';
-        html += '<tbody style="display: block; width: 100%;">';
-            for(row = 0; row < ThisMap.length; row++){
-                html += '<tr style="display: block; width: 100%;">';
-                    for(column = 0; column < ThisMap[row].length; column++){
-                        html += '<td id="' + alphabet[row] + (column + 1) + '" ' + ((ThisMap[row][column] == 0) ? 'class="impassible"' : '') + ' style="width: 72px; text-align: center; padding: 10px; border: solid 1px black;">' + alphabet[row] + (column + 1) + '</td>';
-                    }
-                html += '</tr>';
-            }
-        html += '</tbody>';
-    html += '</table>';
-    $('body').append(html);
+    Map('3');
 }
 
 function draw() {
